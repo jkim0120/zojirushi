428f7d3a493bcda77b559a858f0e4794b1c675e6
Joseph Kim <jkim0120@hotmail.com>
2015-08-19 06:03:41 -0400

    fuckin work

--- lights.rb
+++ lights.rb
@@ -5,20 +5,20 @@ require 'ruby_spark'
 enable :sessions
 
 get '/' do
-  # @core = RubySpark::Core.new('54ff71066678574924590267', '6dfc7bb0441d7fe1712bdf6e664e49bd2ef21a91')
-  # @light_status = if @core.variable('lightOn') == 0
-  #                   'off'
-  #                 else
-  #                   'on'
-  #                 end
+  @core = RubySpark::Core.new('54ff71066678574924590267', '6dfc7bb0441d7fe1712bdf6e664e49bd2ef21a91')
+  @light_status = if @core.variable('lightOn') == 0
+                    'off'
+                  else
+                    'on'
+                  end
 
   erb :index
 end
 
 post '/lights' do
-  # @core = RubySpark::Core.new('54ff71066678574924590267', '6dfc7bb0441d7fe1712bdf6e664e49bd2ef21a91')
-  # @status = params[:status]
-  # @core.function('toggleLight', @status)
+  @core = RubySpark::Core.new('54ff71066678574924590267', '6dfc7bb0441d7fe1712bdf6e664e49bd2ef21a91')
+  @status = params[:status]
+  @core.function('toggleLight', @status)
 
   redirect to('/')
 end
