ca55e2be85f75458af3e28cd78c92ce171cb0074
Joseph Kim <jkim0120@hotmail.com>
2015-09-05 05:35:23 -0400

    Ajaxified

--- lights.rb
+++ lights.rb
@@ -6,7 +6,7 @@ enable :sessions
 
 get '/' do
   @core = RubySpark::Core.new('54ff71066678574924590267', '6dfc7bb0441d7fe1712bdf6e664e49bd2ef21a91')
-  @light_status = if @core.variable('on') == 0
+  @cook_status = if @core.variable('on') == 0
                     'off'
                   else
                     'on'
@@ -15,7 +15,7 @@ get '/' do
   erb :index
 end
 
-post '/lights' do
+post '/cook' do
   @core = RubySpark::Core.new('54ff71066678574924590267', '6dfc7bb0441d7fe1712bdf6e664e49bd2ef21a91')
   @status = params[:status]
   @core.function('toggleOn', @status)
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,25 @@
-console.log('hello')
+$(function() {
+
+  $('#submit').on('click', function(e) {
+    e.preventDefault()
+
+    var val = $(".options input[type='radio']:checked").val()
+
+    $.ajax({
+      url: '/cook',
+      type: 'POST',
+      data: {
+        status: val
+      },
+      success: function() {
+        if (val === 'on') {
+          $('.keep-warm').addClass('inactive')
+          $('.cooking').removeClass('inactive')
+        } else {
+          $('.keep-warm').removeClass('inactive')
+          $('.cooking').addClass('inactive')
+        }
+      }
+    })
+  })
+})
